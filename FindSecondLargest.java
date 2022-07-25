e07e26b9d70d0e45079d67c0b9319713a63566fb
shitallotke <108998809+shitallotke@users.noreply.github.com>
2022-07-25 16:41:22 -0500

    Add files via upload

--- FindSecondLargest.java
+++ FindSecondLargest.java
@@ -9,16 +9,22 @@ public class FindSecondLargest {
 		int[] data = {3,2,11,4,6,7};
 		int dataLenght = data.length;
 		System.out.println("Length of the Array : "+dataLenght);
-		System.out.print("Sorted Array Elements : ");
 		Arrays.sort(data);
+		System.out.print("Sorted Array Elements : ");
+		
 		for(int i=0 ; i<dataLenght ; i++)
 		{
 			System.out.print(data[i] + " ");
 		}
 		System.out.println();
-		System.err.println("Second Largest Element : "+data[dataLenght-2]);
+		System.err.print("Second Largest Element : "+data[dataLenght-2]);
 		
 		
 	}
 
 }
+/*output:
+Length of the Array : 6
+Sorted Array Elements : 2 3 4 6 7 11 
+Second Largest Element : 7
+*/
